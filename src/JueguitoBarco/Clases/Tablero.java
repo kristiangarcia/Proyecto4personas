525cff98436f4010d7bd6d6d3268a8a8d5b8c920
alaslibress <jandroman23@gmail.com>
2025-02-04 08:47:27 +0100

    Clase Tablero con únicamente constructores y test sin rellenar

---
+++ src/JueguitoBarco/Clases/Tablero.java
@@ -0,0 +1,21 @@
+package JueguitoBarco.Clases;
+
+public class Tablero {
+    //Variables de instancia
+    private int totalFilas;
+    private int totalColumnas;
+    private Flota flota;
+
+    //Constructor
+    public Tablero(int f, int c){ //Constructor principal
+        throw new UnsupportedOperationException("Class not programing");
+    }
+
+    public Tablero(int n){ //Constructor secundario ligado al principal (1)
+        throw new UnsupportedOperationException("Class not programing");
+    }
+
+    public Tablero(){ //Constructor secundario (2)
+        throw new UnsupportedOperationException("Class not programing");
+    }
+}
---
+++ src/JueguitoBarco/Clases/TableroTest.java
@@ -0,0 +1,14 @@
+package JueguitoBarco.Clases;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class TableroTest {
+    @Test
+    public void Test1(){
+
+    }
+    public void Test2(){
+
+    }
+}
